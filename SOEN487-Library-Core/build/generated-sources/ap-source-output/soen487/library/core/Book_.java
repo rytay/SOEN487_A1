1c8a81f308af051cc28f269b607388a6f0f60928
Ryan <rytay123@gmail.com>
2020-02-11 18:14:09 -0500

    Completed service class

--- SOEN487-Library-Core/build/generated-sources/ap-source-output/soen487/library/core/Book_.java
+++ SOEN487-Library-Core/build/generated-sources/ap-source-output/soen487/library/core/Book_.java
@@ -4,12 +4,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-11T17:32:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-11T18:13:19")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
     public static volatile SingularAttribute<Book, String> author;
-    public static volatile SingularAttribute<Book, Integer> isbn;
+    public static volatile SingularAttribute<Book, String> isbn;
     public static volatile SingularAttribute<Book, String> description;
     public static volatile SingularAttribute<Book, String> publisher;
     public static volatile SingularAttribute<Book, Integer> id;
