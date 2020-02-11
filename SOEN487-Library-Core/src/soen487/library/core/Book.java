873e136153d6b86d9cc8c1b356a83a39484a8ac7
Ryan <rytay123@gmail.com>
2020-02-11 17:04:47 -0500

    initialized repo with allprojects

---
+++ SOEN487-Library-Core/src/soen487/library/core/Book.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soen487.library.core;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Xavier Vani-Charron
+ */
+@Entity
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String description;
+    private Integer isbn;
+    private String author;
+    private String publisher;
+    
+    
+    public Book(){
+        this.id = 0;
+        this.title = null;
+        this.description = null;
+        this.author=null;
+        this.publisher=null;
+    }
+    
+    public Book(int id, String title, String description, String isbn, String author, String publisher){
+        this.id=id;
+        this.title=title;
+        this.description=description;
+        this.isbn = Integer.parseInt(isbn);
+        this.author = author;
+        this.publisher=publisher;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Book)) {
+            return false;
+        }
+        Book other = (Book) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Integer isbn){
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    
+}
---
+++ SOEN487-Library-System/src/soen487/library/system/Library.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soen487.library.system;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import soen487.library.core.Book;
+
+/**
+ *
+ * @author Xavier Vani-Charron, Ryan Taylor
+ */
+
+//TODO: Should we accept Book objects as arguments or the parameters themselves?
+public class Library {
+    private static Library library_instance = null;
+    private static ConcurrentHashMap bookList = null;
+    private static final AtomicInteger sequence = new AtomicInteger(0);
+
+    private Library(){
+        bookList = new ConcurrentHashMap<Integer,String>();
+    }
+    
+    public static Library getInstance(){
+        if(library_instance == null){
+            library_instance = new Library();
+        }
+        return library_instance;
+    }
+    
+    //Probably dont need this considering two ids may be the same depending on what the last id was for the paramter hashmap
+    public Library(ConcurrentHashMap bookList) {
+        this.bookList = bookList;
+    }
+    
+    public void create(String title, String description, String isbn, String author, String publisher){
+        
+        Book newBook = new Book(sequence.incrementAndGet() ,title, description, isbn, author, publisher);
+	bookList.put(newBook.getId(), newBook);
+        
+    }
+    
+    //Will be null if id not present in bookList, otherwise will return the book but it must be cast as book when returned.
+    public Book delete(Integer id){
+	return (Book)bookList.remove(id);
+    }
+    
+    //TODO:Should we add a book if not in the hashmap? This implementation does not. 
+    public void update(Book updatedBook){
+	
+	int id = updatedBook.getId();
+	if(bookList.containsKey(id))
+	    bookList.put(id, updatedBook);
+	//Uncomment for creating a new book if it does not exist
+	/*else{
+	    this.create(updatedBook)
+	}
+	*/
+	
+    }
+    
+    //If id is null return all the books, otherwise return matching book or empty ArrayList if not present
+    public ArrayList<Book> read(Integer id){
+        
+	ArrayList<Book> result = new ArrayList<>();
+	
+	if(id == null){
+	    result.addAll(bookList.values());
+	}
+	else{
+	    if(bookList.containsKey(id))
+		result.add((Book)bookList.get(id));
+	}
+	return result;
+    }
+}
