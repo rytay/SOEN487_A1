1b1288ba0ee3da6938f021cae0fed2cc7dcd989f
jmassabni <jonmass59@gmail.com>
2020-02-13 18:18:55 -0500

    Completed the Console app
    All options in the console menu are working

--- SOEN487-Library-Client/src/java/soen487/library/client/main.java
+++ SOEN487-Library-Client/src/java/soen487/library/client/main.java
@@ -59,12 +59,45 @@ public class main {
               break;
             case 4:
               System.out.println("input was 4");
+              System.out.println("Enter book title");
+              String title = kb.next();
+              System.out.println("Enter book description");
+              String desc = kb.next();
+              System.out.println("Enter book isbn");
+              String isbn = kb.next();
+              System.out.println("Enter book author");
+              String author = kb.next();
+              System.out.println("Enter book publisher");
+              String publ = kb.next();
+
+              Object addBook = client.post(title, desc, isbn, author, publ);
+              System.out.println("Result : " + addBook);
               break;
             case 5:
               System.out.println("input was 5");
+              System.out.println("Enter book ID");
+              String bookID = kb.next();
+              
+              System.out.println("Enter new book title");
+              String newtitle = kb.next();
+              System.out.println("Enter new book description");
+              String newdesc = kb.next();
+              System.out.println("Enter new book isbn");
+              String newisbn = kb.next();
+              System.out.println("Enter new book author");
+              String newauthor = kb.next();
+              System.out.println("Enter new book publisher");
+              String newpubl = kb.next();
+              
+              Object updateBook = client.editBook(bookID, newtitle, newdesc, newisbn, newauthor, newpubl);
+              System.out.println("Result : " + updateBook);
               break;
             case 6:
               System.out.println("input was 6");
+              System.out.println("Enter book ID");
+              String select5 = kb.next();
+              Object deleteBook = client.deleteBook(select5);
+              System.out.println(deleteBook);
               break;
             default:
               System.out.println("User quit");
